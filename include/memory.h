78efa86fab4ce192db08d98dbd68cf065c445a9a
Perpetu4l <vaibhav.0756@gmail.com>
2026-01-01 11:34:15 +0530

    initial commit

---
+++ include/memory.h
@@ -0,0 +1,16 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <list>
+using namespace std;
+
+struct Block {
+    int start;
+    int size;
+    bool free;
+};
+
+void init_memory(int total_size);
+void dump_memory();
+
+#endif
